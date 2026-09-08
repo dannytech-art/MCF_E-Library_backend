60173c5f08d4f7280e271ab37776e58efa55275c
dannytech-art <dobi44134@gmail.com>
2026-09-08 16:02:44 +0100

    updated env format

--- src/services/googledriveservics.js
+++ src/services/googledriveservics.js
@@ -79,11 +79,15 @@
 
 const { google } = require("googleapis");
 
+const privateKey = process.env.GOOGLE_PRIVATE_KEY
+  ?.replace(/\\n/g, "\n")
+  .replace(/^"|"$/g, "");
+
 const auth = new google.auth.GoogleAuth({
   credentials: {
     project_id: process.env.GOOGLE_PROJECT_ID,
     private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    private_key: privateKey,
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
   },
